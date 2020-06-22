6f03e15071f00ba8d74180d0fb45b877cccc60dd
johnklima <klima.de.sa@gmail.com>
2020-06-22 13:00:31 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/cityarts/lisboasoa2020/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cityarts.lisboasoa2020
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lisboa Soa 2020',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Lisboa Soa 2020'),
+    );
+  }
+}
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return ListTile(
+      title: Row(
+        children: [
+          Expanded(
+            child: Text(
+              document['name'],
+              style: Theme.of(context).textTheme.headline,
+            ),
+          ),
+          Container(
+            decoration: const BoxDecoration(
+              color: Color(0xffddddff),
+            ),
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              document['votes'].toString(),
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ),
+        ],
+      ),
+      onTap: () {
+        //dirty the file :)
+        int incr = 0;
+        Firestore.instance.runTransaction((transaction) async {
+
+          DocumentSnapshot freshSnap =
+          await transaction.get(document.reference);
+          await transaction.update(freshSnap.reference, {
+            'votes': freshSnap['votes'] + 1,
+          });
+          incr = freshSnap['votes'];
+          //add document?
+          Firestore.instance.collection("LocationAudio").add(
+              {
+                "name" : "new data " + incr.toString() ,
+                "votes" : incr,
+              });
+
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: StreamBuilder(
+          stream: Firestore.instance.collection('LocationAudio').snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return const Text('Loading...');
+            return ListView.builder(
+              itemExtent: 80.0,
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) =>
+                  _buildListItem(context, snapshot.data.documents[index]),
+            );
+          }),
+    );
+  }
+}
