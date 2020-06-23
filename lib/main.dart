156813ab4c843648d380dffc33956089fae93052
johnklima <klima.de.sa@gmail.com>
2020-06-23 14:30:50 +0100

    split screen map and database
    major progres!!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
-void main() {
+/*void main() {
   runApp(const MyApp());
 }
+*/
 
-class MyApp extends StatelessWidget {
-  const MyApp();
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+
+
     return MaterialApp(
+
       title: 'Lisboa Soa 2020',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+
       home: const MyHomePage(title: 'Lisboa Soa 2020'),
+
+
+      /*
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 20.0,
+          ),
+        ),
+      ),
+      */
     );
   }
-}
 
+  }
+
+
+GoogleMapController mapController;
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
+
+
+  final LatLng _center = const LatLng(38.722586, -9.137905);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
     return ListTile(
@@ -78,7 +116,44 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: StreamBuilder(
+      body:
+
+          Column(
+              children: [
+                SizedBox(
+                    width: MediaQuery.of(context).size.width,  // or use fixed size like 200
+                    height: MediaQuery.of(context).size.height / 3,
+                    child: GoogleMap(
+                      mapType: MapType.normal,
+                      initialCameraPosition: CameraPosition(target: _center, zoom: 15),
+                    )),
+
+              SizedBox(
+                width: MediaQuery.of(context).size.width,  // or use fixed size like 200
+                height: MediaQuery.of(context).size.height / 3,
+
+                child:
+                    StreamBuilder(
+                    stream: Firestore.instance.collection('LocationAudio').snapshots(),
+                    builder: (context, snapshot) {
+                    if (!snapshot.hasData) return const Text('Loading...');
+                    return ListView.builder(
+                    itemExtent: 80.0,
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index) =>
+                    _buildListItem(context, snapshot.data.documents[index]),
+                    );
+                    })
+
+              ),
+
+              ]
+          )
+
+    );
+
+    /*
+      StreamBuilder(
           stream: Firestore.instance.collection('LocationAudio').snapshots(),
           builder: (context, snapshot) {
             if (!snapshot.hasData) return const Text('Loading...');
@@ -89,6 +164,10 @@ class MyHomePage extends StatelessWidget {
                   _buildListItem(context, snapshot.data.documents[index]),
             );
           }),
-    );
+      */
+
+
+
+
   }
 }
