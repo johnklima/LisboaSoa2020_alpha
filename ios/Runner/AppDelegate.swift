70822c1a6e93d086bdef8a30917137db555ab98b
johnklima <klima.de.sa@gmail.com>
2020-07-12 14:48:37 +0100

    good progress on iOS build
    fixed some plist bullshit, maps working, webview working, still need to get audio to play backe (more plist crap)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCps1_vYn5X_zR8krV0kcYuPMC4FXPAglc")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
