17986ee53fac4a526394bae284c1b0e8fa633b23
JohnH <51337462+JohnHauge@users.noreply.github.com>
2020-07-13 14:47:41 +0200

    Background & Buttons
    Have made some progress on the visuals.

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -5,7 +5,6 @@ import "package:flutter/material.dart";
 // Here we can keep all our button widgets, this will help us keep our
 // scripts much cleaner.
 
-
 // Navigates to a new page.
 class NavigateTo extends StatelessWidget {
   var pageName;
@@ -16,23 +15,29 @@ class NavigateTo extends StatelessWidget {
     //The container info might be best to keep in the other widget
     //rather than in here, perhaps change this to only a Raised Button.
     return Container(
-      margin: EdgeInsets.only(
-      top: 20,
-      bottom:  20,
-      left: 100,
-      right: 100,
-    ),
+      margin: EdgeInsets.all(15),
+      child: ButtonTheme(
+        minWidth: 250,
+        height: 60,
+        child: RaisedButton(
+          color: Colors.white,
+          child: Text(
+              pageName.toString(),
+          style: Theme.of(context).textTheme.bodyText1,
 
-      child: RaisedButton(
-        color: Colors.lightBlueAccent[100],
-        child: Text(pageName.toString()),
-        textColor: Colors.black45,
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => pageName),
-          );
-        },
+          ),
+
+          textColor: Colors.lightGreen,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => pageName),
+            );
+          },
+        ),
       ),
     );
   }
@@ -42,11 +47,9 @@ class NavigateTo extends StatelessWidget {
 /// but it's not working when having the widget in here for
 /// some reason.
 class NavigateBack extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
-
       child: RaisedButton(
         child: Text("Go Back"),
         onPressed: () {
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:file/file.dart';
 import 'package:file/local.dart';
+import 'package:flutter/widgets.dart';
 
 //Internal Packages
 import 'buttons.dart';
@@ -25,7 +26,19 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Lisboa Soa 2020',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightGreen,
+
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
+          bodyText1: TextStyle(
+              fontSize: 20,
+              fontWeight:
+              FontWeight.bold,
+            color: Colors.lightGreen,
+
+          ),
+        )
+
       ),
       home: HomePage(),
 
@@ -52,47 +65,53 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
       // List View widget seams like a nice menu widget,
       // this is very basic and can be customized a lot further.
 
       //https://api.flutter.dev/flutter/widgets/ListView-class.html
-      body: ListView(
+      body: Stack(
         children: <Widget>[
-          Container(
-            height: 80,
-            color: Colors.yellow[100],
-            //I have added a buttons.dart where I keep the button widgets,
-            //NavigateTo is such a button, look in buttons.dart for more.
-            child: NavigateTo(DataAndMap()),
+          Image(
+            image: AssetImage("assets/Graphic/LisboaSoa_Background.png"),
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.fitWidth,
           ),
+
           Container(
-            height: 80,
-            color: Colors.yellow[200],
-            child: NavigateTo(AudioRecorder()),
+            height: double.infinity,
+            width: double.infinity,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+
+//I have added a buttons.dart where I keep the button widgets,
+//NavigateTo is such a button, look in buttons.dart for more.
+                    child: NavigateTo(DataAndMap()),
+                  ),
+                  Container(
+
+                    child: NavigateTo(AudioRecorder()),
+                  ),
+                  Container(
+                    child: NavigateTo(Calendar()),
+                  ),
+                  Container(
+                    child: NavigateTo(VideoPlayerScreen()),
+                  ),
+                  Container(
+                    child: NavigateTo(FireAudio()),
+                  ),
+                  Container(
+                    child: NavigateTo(Website()),
+                  ),
+                ],
+              ),
+            ),
           ),
-          Container(
-            height: 80,
-            color: Colors.yellow[3 + 00],
-            child: NavigateTo(Calendar()),
-          ),
-          Container(
-            height: 80,
-            color: Colors.yellow[3 + 00],
-            child: NavigateTo(VideoPlayerScreen()),
-          ),
-          Container(
-            height: 80,
-            color: Colors.yellow[3 + 00],
-            child: NavigateTo(FireAudio()),
-          ),
-          Container(
-            height: 80,
-            color: Colors.yellow[3 + 00],
-            child: NavigateTo(Website()),
-          ),
+
         ],
       ),
     );
@@ -107,4 +126,42 @@ final LocalFileSystem localFileSystem;
 // Calls a function from a imported package to get the local directory.
 MyApp({localFileSystem})
     : this.localFileSystem = localFileSystem ?? LocalFileSystem();
+*/
+/*
+body: ListView(
+children: <Widget>[
+Container(
+height: 80,
+color: Colors.yellow[100],
+//I have added a buttons.dart where I keep the button widgets,
+//NavigateTo is such a button, look in buttons.dart for more.
+child: NavigateTo(DataAndMap()),
+),
+Container(
+height: 80,
+color: Colors.yellow[200],
+child: NavigateTo(AudioRecorder()),
+),
+Container(
+height: 80,
+color: Colors.yellow[3 + 00],
+child: NavigateTo(Calendar()),
+),
+Container(
+height: 80,
+color: Colors.yellow[3 + 00],
+child: NavigateTo(VideoPlayerScreen()),
+),
+Container(
+height: 80,
+color: Colors.yellow[3 + 00],
+child: NavigateTo(FireAudio()),
+),
+Container(
+height: 80,
+color: Colors.yellow[3 + 00],
+child: NavigateTo(Website()),
+),
+],
+),
 */
