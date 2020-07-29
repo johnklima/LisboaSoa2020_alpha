11c24a9668b37a7902ec3013cec65282b34d7905
JohnH <51337462+JohnHauge@users.noreply.github.com>
2020-07-29 21:29:48 +0200

    Recorder now works

--- lib/record.dart
+++ lib/record.dart
@@ -1,18 +1,42 @@
+import 'dart:async';
+import 'dart:convert'; // not sure if this is needed.
+import 'dart:io' as io;
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_audio_recorder/flutter_audio_recorder.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter_guid/flutter_guid.dart';
 
 import 'buttons.dart';
-import 'recorder.dart';
+import 'audioFiles.dart';
 
 class Recorder extends StatefulWidget {
   @override
-  _RecorderState createState() => new _RecorderState();
+  State<StatefulWidget> createState() => new _RecorderState();
 }
 
 class _RecorderState extends State<Recorder> {
+  FlutterAudioRecorder _recorder;
+  Recording _current;
+  RecordingStatus _currentStatus = RecordingStatus.Unset;
 
-  final recorder = new ExampleRecorder();
+  var DocDir;
+
+  // variables stored for sending to the json on save.
+  String guid;
+  Position pos;
+  String dur;
+  String loc;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _init();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -133,7 +157,8 @@ class _RecorderState extends State<Recorder> {
                     Center(
                       /// time audio has been played.
                       child: Container(
-                        child: Text("00:00:00",
+                        child: new Text(
+                            "${_current?.duration?.toString()}",
                             style: Theme.of(context).textTheme.headline2),
                       ),
                     ),
@@ -147,9 +172,11 @@ class _RecorderState extends State<Recorder> {
                     margin: EdgeInsets.all(5),
                     child: Column(
                       children: <Widget>[
-                        RecorderButton(
-                            "record", 'assets/Icons/LisboaSoa_Icon_Record.png', this
-                        ),
+
+                        recordButton(),
+
+                            //"record", 'assets/Icons/LisboaSoa_Icon_Record.png', this
+
 
                         RecorderButton(
                             "stop", 'assets/Icons/LisboaSoa_Icon_Stop.png', this
@@ -172,16 +199,175 @@ class _RecorderState extends State<Recorder> {
     );
   }
 
+  Widget recordButton() {
+
+    var _widgetToReturn = RecorderButton(
+        "new", 'assets/Icons/LisboaSoa_Icon_Record.png', this
+    );
+
+    if (_currentStatus == RecordingStatus.Initialized){
+      _widgetToReturn = RecorderButton(
+          "record", 'assets/Icons/LisboaSoa_Icon_Record.png', this
+      );
+    }
+    else if (_currentStatus == RecordingStatus.Recording){
+      _widgetToReturn = RecorderButton(
+          "pause", 'assets/Icons/LisboaSoa_Icon_Record.png', this
+      );
+    }
+    else if (_currentStatus == RecordingStatus.Paused){
+      _widgetToReturn = RecorderButton(
+          "resume", 'assets/Icons/LisboaSoa_Icon_Record.png', this
+      );
+    }
+    else if (_currentStatus == RecordingStatus.Stopped){
+      _widgetToReturn = RecorderButton(
+          "new", 'assets/Icons/LisboaSoa_Icon_Record.png', this
+      );
+    }
+    return _widgetToReturn;
+  }
+
   void theOnPress(String st){
     if (st == "record"){
-      print(st);
-      
+      _start();
     } else if (st == "stop"){
-      print(st);
+      _currentStatus != RecordingStatus.Unset ? _stop() : null;
     } else if (st == "play"){
-      print(st);
+      onPlayAudio();
     } else if (st == "save"){
-      print(st);
+      saveToJson();
+    } else if (st == "pause"){
+      _pause();
+    } else if (st == "resume"){
+      _resume();
+    } else if (st == "new"){
+      _init();
     }
   }
+
+  //Function for initiating a new recording, making a new guid etc.
+  _init() async {
+    try {
+      if (await FlutterAudioRecorder.hasPermissions) {
+        io.Directory appDocDirectory;
+//        io.Directory appDocDirectory = await getApplicationDocumentsDirectory();
+        if (io.Platform.isIOS) {
+          appDocDirectory = await getApplicationDocumentsDirectory();
+        } else {
+          appDocDirectory = await getExternalStorageDirectory();
+        }
+
+        pos = await Geolocator()
+            .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+        Guid iD = Guid.newGuid;
+        guid = iD.value;
+        // can add extension like ".mp4" ".wav" ".m4a" ".aac"
+        loc = appDocDirectory.path + "/" + guid;
+
+        DocDir = appDocDirectory.path;
+        print("This is the Doc Dir !!!! " + DocDir);
+
+        // .wav <---> AudioFormat.WAV
+        // .mp4 .m4a .aac <---> AudioFormat.AAC
+        // AudioFormat is optional, if given value, will overwrite path extension when there is conflicts.
+        _recorder = FlutterAudioRecorder(loc);
+        //_current.path = customPath;
+
+        await _recorder.initialized;
+        // after initialization
+        var current = await _recorder.current(channel: 0);
+        print(current);
+        // should be "Initialized", if all working fine
+        setState(() {
+          _current = current;
+          _currentStatus = current.status;
+          print(_currentStatus);
+        });
+      } else {
+        Scaffold.of(context).showSnackBar(
+            new SnackBar(content: new Text("You must accept permissions")));
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  //Starts recording the audio, this loops while user is recording.
+  _start() async {
+    try {
+      await _recorder.start();
+      var recording = await _recorder.current(channel: 0);
+      setState(() {
+        _current = recording;
+      });
+
+      const tick = const Duration(milliseconds: 50);
+      new Timer.periodic(tick, (Timer t) async {
+        if (_currentStatus == RecordingStatus.Stopped) {
+          t.cancel();
+        }
+
+        var current = await _recorder.current(channel: 0);
+        // print(current.status);
+        setState(() {
+          _current = current;
+          _currentStatus = _current.status;
+        });
+
+        // if statement to stop recording if it goes over 30 sec.
+
+        // ignore: null_aware_before_operator
+        if (_current?.duration?.inSeconds > 30.00) {
+          _stop();
+        }
+
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  //Resumes the recording
+  _resume() async {
+    await _recorder.resume();
+    setState(() {});
+  }
+
+  //Pauses the recording
+  _pause() async {
+    await _recorder.pause();
+    setState(() {});
+  }
+  //Stops the recording and stores it in the local drive.
+  //Perhaps move this to the "Save" function.
+  _stop() async {
+    var result = await _recorder.stop();
+    print("Stop recording: ${result.path}");
+    print("Stop recording: ${result.duration}");
+    //File file = MyApp().localFileSystem.file(result.path);
+    //print("File length: ${await file.length()}");
+    setState(() {
+      _current = result;
+      _currentStatus = _current.status;
+      dur = _current?.duration.toString();
+    });
+  }
+
+  //Play the recorder audio.
+  //This is what we should use in the audio lib I suppose.
+  void onPlayAudio() async {
+    AudioPlayer audioPlayer = AudioPlayer();
+    await audioPlayer.play(_current.path, isLocal: true);
+  }
+
+  //Save function, currently only stores the info in the json file.
+  //I haven't really found the json file yet so I am not sure if it works,
+  //or if this is the correct way to send it to the json.
+  void saveToJson() {
+    AudioFiles audioFiles = AudioFiles(guid, pos.toString(), dur, loc);
+    print(audioFiles.toJson());
+  }
 }
+
+
